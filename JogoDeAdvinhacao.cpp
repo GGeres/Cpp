4f265c78c6b4da7640900c8093f1331a31782b9a
Gustavo Jorge Geres <ezio.geres@gmail.com>
2023-08-28 17:16:37 -0300

    Update 2

--- JogoDeAdvinhacao.cpp
+++ JogoDeAdvinhacao.cpp
@@ -5,4 +5,7 @@ int main(){
     cout<< "************************************" << endl;
     cout<< "* Bem-Vindos ao Jogo de Advinhação *" << endl;
     cout<< "************************************" << endl;
+
+    int numero_secreto = 42;
+    cout<< "O numero secreto é "<<numero_secreto<<". Não conte a ninguém."<<endl;
 }
