63e60cde989ee52148552740d3e752de2b8a0199
Gustavo Jorge Geres <ezio.geres@gmail.com>
2023-08-28 17:35:41 -0300

    Update 4

--- JogoDeAdvinhacao.cpp
+++ JogoDeAdvinhacao.cpp
@@ -3,17 +3,24 @@ using namespace std;
 
 int main(){
     cout<< "************************************" << endl;
-    cout<< "* Bem-Vindos ao Jogo de Advinhação *" << endl;
+    cout<< "* Bem-Vindos ao Jogo de Advinhacao *" << endl;
     cout<< "************************************" << endl;
 
     int numero_secreto = 42;
-    cout<< "O numero secreto é "<<numero_secreto<<". Não conte a ninguém."<<endl;
+    cout<< "O numero secreto e "<<numero_secreto<<". Nao conte a ninguem."<<endl;
 
     int chute;
-    cout<<"Qual é o seu chute? ";
+    cout<<"Qual e o seu chute? ";
     cin>> chute;
 
-    cout<<"O valor do seu chute é:"<<chute<< endl;
+    cout<<"O valor do seu chute e:"<<chute<< endl;
+
+    if(chute == numero_secreto){
+        cout<<"Voce acertou o numero secreto"
+    }
+    else{
+        cout<<"Voce errou, tente novamente. "
+    }
 
 
 }
