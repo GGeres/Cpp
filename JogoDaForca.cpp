16887c6e26e7faf99df77e04c83bcde3ac00249d
Gustavo Jorge Geres <ezio.geres@gmail.com>
2023-08-30 19:30:56 -0300

    Update 15

--- JogoDaForca.cpp
+++ JogoDaForca.cpp
@@ -1,8 +1,10 @@
 #include<iostream>
 #include <map>
+#include <vector>
 using namespace std;
 const string P_SECRETA = "MELANCIA";
 map<char, bool> chutou;
+vector <char> chutes_errados;
 
 bool letra_existe (char chute){
     for (char letra : P_SECRETA){
@@ -13,12 +15,41 @@ bool letra_existe (char chute){
     return false;    
 }
 
+bool nao_acertou(){
+    for(char letra : P_SECRETA){
+        if (!chutou[letra]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool nao_enforcou(){
+    return chutes_errados.size() < 5;
+}    
+
 int main(){
-    
-    bool nao_acerto = true;
+    cout<<"***************************************"<< endl;
+    cout<<"******        Jogo da Forca       *****"<< endl;
+    cout<<"***************************************"<< endl;
+    cout<<endl;
+
+    bool nao_acertou = true;
     bool nao_enforcou = true;
 
-    while(nao_acerto && nao_enforcou){
+    cout<<"Seu chute: ";
+    char chute;
+    cin>> chute;
+
+    chutou[chute] = chute;
+
+    while(nao_acertou() && nao_enforcou()){
+        cout << "Chutes errados: ";
+        for(char letra: chutes_errados){
+            cout << letra << " ";
+        }
+        cout << endl;
+        
         for (char letra : P_SECRETA){
             if(chutou[letra]){
                 cout<< letra<<" ";
@@ -39,6 +70,8 @@ int main(){
         }
         else{
             cout<<"Voce errou. Seu chute nao esta na palavra."<< endl;
+            chutes_errados.push_back(chute);
         }
+        cout<<endl;
     }
 }
