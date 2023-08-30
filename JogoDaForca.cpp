2264b88397f7719bd1ad23850a3c5b3c2d18a6ee
Gustavo Jorge Geres <ezio.geres@gmail.com>
2023-08-30 18:45:25 -0300

    Update 10

--- JogoDaForca.cpp
+++ JogoDaForca.cpp
@@ -1,7 +1,32 @@
 #include<iostream>
 using namespace std;
+const string P_SECRETA = "MELANCIA";
+
+bool letra_existe (char chute){
+    for (int i = 0; i < P_SECRETA.size(); i++){
+        if(chute == P_SECRETA[i]){
+            return true;
+        }
+    }
+    return false;    
+}
 
 int main(){
-    string p_secreta = "MELANCIA";
-    cout<< p_secreta << endl;
+    
+    cout<< P_SECRETA << endl;
+
+    bool nao_acerto = true;
+    bool nao_enforcou = true;
+
+    while(nao_acerto && nao_enforcou){
+        char chute;
+        cin >> chute;
+
+        if(letra_existe(chute)){
+            cout<<"Voce acertou!! Seu chute esta na palavra."<<endl;
+        }
+        else{
+            cout<<"Voce errou. Seu chute nao esta na palavra."<< endl;
+        }
+    }
 }
